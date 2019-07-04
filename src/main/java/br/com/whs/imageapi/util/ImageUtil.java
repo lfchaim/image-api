987460f4d89c3b72326ffa632f01af8e3b5fa7e5
Luis Fernando Chaim <luis.chaim@kroton.com.br>
2019-07-04 08:23:11 -0300

    Atualizacao optimizeByFile

--- src/main/java/br/com/whs/imageapi/util/ImageUtil.java
+++ src/main/java/br/com/whs/imageapi/util/ImageUtil.java
@@ -75,60 +75,6 @@ public class ImageUtil {
 		return sourceImage.getSubimage(left, top, right - left, bottom - top);
 	}
 
-	public static BufferedImage getCroppedImage(BufferedImage source, double tolerance) {
-		// Get our top-left pixel color as our "baseline" for cropping
-		int baseColor = source.getRGB(0, 0);
-
-		int width = source.getWidth();
-		int height = source.getHeight();
-
-		int topY = Integer.MAX_VALUE, topX = Integer.MAX_VALUE;
-		int bottomY = -1, bottomX = -1;
-		for (int y = 0; y < height; y++) {
-			for (int x = 0; x < width; x++) {
-				if (colorWithinTolerance(baseColor, source.getRGB(x, y), tolerance)) {
-					if (x < topX)
-						topX = x;
-					if (y < topY)
-						topY = y;
-					if (x > bottomX)
-						bottomX = x;
-					if (y > bottomY)
-						bottomY = y;
-				}
-			}
-		}
-
-		BufferedImage destination = new BufferedImage((bottomX - topX + 1), (bottomY - topY + 1),
-				BufferedImage.TYPE_INT_ARGB);
-
-		destination.getGraphics().drawImage(source, 0, 0, destination.getWidth(), destination.getHeight(), topX, topY,
-				bottomX, bottomY, null);
-
-		return destination;
-	}
-
-	private static boolean colorWithinTolerance(int a, int b, double tolerance) {
-		int aAlpha = (int) ((a & 0xFF000000) >>> 24); // Alpha level
-		int aRed = (int) ((a & 0x00FF0000) >>> 16); // Red level
-		int aGreen = (int) ((a & 0x0000FF00) >>> 8); // Green level
-		int aBlue = (int) (a & 0x000000FF); // Blue level
-
-		int bAlpha = (int) ((b & 0xFF000000) >>> 24); // Alpha level
-		int bRed = (int) ((b & 0x00FF0000) >>> 16); // Red level
-		int bGreen = (int) ((b & 0x0000FF00) >>> 8); // Green level
-		int bBlue = (int) (b & 0x000000FF); // Blue level
-
-		double distance = Math.sqrt((aAlpha - bAlpha) * (aAlpha - bAlpha) + (aRed - bRed) * (aRed - bRed)
-				+ (aGreen - bGreen) * (aGreen - bGreen) + (aBlue - bBlue) * (aBlue - bBlue));
-
-		// 510.0 is the maximum distance between two colors
-		// (0,0,0,0 -> 255,255,255,255)
-		double percentAway = distance / 510.0d;
-
-		return (percentAway > tolerance);
-	}
-	
 	public static BufferedImage autoCrop(final BufferedImage image, final int fuzziness) throws IOException {
 		final Color color = new Color(image.getRGB(0, 0));
 		boolean stop = false;
@@ -204,74 +150,122 @@ public class ImageUtil {
 		final BufferedImage cropped = image.getSubimage(cropLeft, cropTop, cropRight - cropLeft, cropBot - cropTop);
 		return cropped;
 	}
-	
-	public static BufferedImage resize(BufferedImage source, int targetW, int targetH) {
 
-        int type = source.getType();
-        BufferedImage target = null;
-        double sx = (double) targetW / source.getWidth();
-        double sy = (double) targetH / source.getHeight();
-        if (sx < sy) {
-            sx = sy;
-            targetW = (int) (sx * source.getWidth());
-        } else {
-            sy = sx;
-            targetH = (int) (sy * source.getHeight());
-        }
+	private static boolean colorWithinTolerance(int a, int b, double tolerance) {
+		int aAlpha = (int) ((a & 0xFF000000) >>> 24); // Alpha level
+		int aRed = (int) ((a & 0x00FF0000) >>> 16); // Red level
+		int aGreen = (int) ((a & 0x0000FF00) >>> 8); // Green level
+		int aBlue = (int) (a & 0x000000FF); // Blue level
 
-        target = new BufferedImage(targetW, targetH, BufferedImage.TYPE_INT_BGR);
+		int bAlpha = (int) ((b & 0xFF000000) >>> 24); // Alpha level
+		int bRed = (int) ((b & 0x00FF0000) >>> 16); // Red level
+		int bGreen = (int) ((b & 0x0000FF00) >>> 8); // Green level
+		int bBlue = (int) (b & 0x000000FF); // Blue level
 
-        Graphics2D g = target.createGraphics();
-        // smoother than exlax:
-        g.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
-                RenderingHints.VALUE_INTERPOLATION_BICUBIC);
-        g.drawRenderedImage(source, AffineTransform.getScaleInstance(sx, sy));
-        g.dispose();
-        return target;
-    }
+		double distance = Math.sqrt((aAlpha - bAlpha) * (aAlpha - bAlpha) + (aRed - bRed) * (aRed - bRed)
+				+ (aGreen - bGreen) * (aGreen - bGreen) + (aBlue - bBlue) * (aBlue - bBlue));
+
+		// 510.0 is the maximum distance between two colors
+		// (0,0,0,0 -> 255,255,255,255)
+		double percentAway = distance / 510.0d;
+
+		return (percentAway > tolerance);
+	}
 	
-	public static BufferedImage resize(BufferedImage source, float ratio) {
-        BufferedImage target = null;
-        double sx = (double) ratio * source.getWidth();
-        double sy = (double) ratio * source.getHeight();
-        int targetW = source.getWidth();
-        int targetH = source.getHeight();
-        if( sx > 0 ) {
-            targetW = (int)sx;
-        } 
-        if( sy > 0 ){
-            targetH = (int)sy;
-        }
-        target = new BufferedImage(targetW, targetH, source.getType());
-        Graphics2D g = target.createGraphics();
-        // smoother than exlax:
-        g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
-        g.drawRenderedImage(source, AffineTransform.getScaleInstance(ratio, ratio));
-        g.dispose();
-        return target;
-    }
+	public static BufferedImage getCroppedImage(BufferedImage source, double tolerance) {
+		// Get our top-left pixel color as our "baseline" for cropping
+		int baseColor = source.getRGB(0, 0);
+
+		int width = source.getWidth();
+		int height = source.getHeight();
+
+		int topY = Integer.MAX_VALUE, topX = Integer.MAX_VALUE;
+		int bottomY = -1, bottomX = -1;
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				if (colorWithinTolerance(baseColor, source.getRGB(x, y), tolerance)) {
+					if (x < topX)
+						topX = x;
+					if (y < topY)
+						topY = y;
+					if (x > bottomX)
+						bottomX = x;
+					if (y > bottomY)
+						bottomY = y;
+				}
+			}
+		}
+
+		BufferedImage destination = new BufferedImage((bottomX - topX + 1), (bottomY - topY + 1),
+				BufferedImage.TYPE_INT_ARGB);
+
+		destination.getGraphics().drawImage(source, 0, 0, destination.getWidth(), destination.getHeight(), topX, topY,
+				bottomX, bottomY, null);
+
+		return destination;
+	}
 	
-	public static BufferedImage toGrayScale( BufferedImage source ) {
-		BufferedImage result = new BufferedImage(
-			source.getWidth(),
-			source.getHeight(),
-            BufferedImage.TYPE_INT_RGB);
-        Graphics2D graphic = result.createGraphics();
-        graphic.drawImage(source, 0, 0, Color.WHITE, null);
-        for (int i = 0; i < result.getHeight(); i++) {
-            for (int j = 0; j < result.getWidth(); j++) {
-                Color c = new Color(result.getRGB(j, i));
-                int red = (int) (c.getRed() * 0.299);
-                int green = (int) (c.getGreen() * 0.587);
-                int blue = (int) (c.getBlue() * 0.114);
-                Color newColor = new Color(
-	                red + green + blue,
-	                red + green + blue,
-	                red + green + blue);
-                result.setRGB(j, i, newColor.getRGB());
-            }
-        }
-        return result;
+	public static String getFormatName( String fileName ) throws IOException {
+		File sourceFile = new File(fileName);
+		ImageInputStream iis = ImageIO.createImageInputStream(sourceFile);
+		Iterator<ImageReader> imageReaders = ImageIO.getImageReaders(iis);
+		ImageReader ir = null;
+		while( imageReaders.hasNext() ) {
+			ir = imageReaders.next();
+		}
+		sourceFile = null;
+		return ir != null ? ir.getFormatName() : null;
+	}
+	
+	public static BufferedImage load( File file ) throws IOException {
+		return ImageIO.read(file);
+	}
+	
+	public static BufferedImage optimize(BufferedImage bufferedImage, float quality, int dpi, int width, int height, boolean gray) throws IOException {
+		File file = new File(optimizeByFile(bufferedImage, quality, dpi, width, height, gray));
+		return load(file);
+	}
+	
+	public static String optimizeByFile(BufferedImage bufferedImage, float quality, int dpi, int width, int height, boolean gray) throws IOException {
+		File outputFile = File.createTempFile("optimizeByFile", ".jpeg");
+		try {
+			/*
+			ImageInputStream iis = ImageIO.createImageInputStream(bufferedImage);
+			Iterator<ImageReader> imageReaders = ImageIO.getImageReaders(iis);
+			ImageReader ir = null;
+			while( imageReaders.hasNext() ) {
+				ir = imageReaders.next();
+				System.out.println("ImageReader: "+ir.getFormatName());
+			}
+			ImageWriter imageWriter = ImageIO.getImageWritersBySuffix(ir.getFormatName().toLowerCase()).next();
+			*/
+			ImageWriter imageWriter = ImageIO.getImageWritersBySuffix("jpeg").next();
+			ImageOutputStream ios = ImageIO.createImageOutputStream(outputFile);
+			imageWriter.setOutput(ios);
+			ImageWriteParam jpegParams = imageWriter.getDefaultWriteParam();
+			
+			jpegParams.setCompressionMode(JPEGImageWriteParam.MODE_EXPLICIT);
+			jpegParams.setCompressionQuality(quality);
+	
+			IIOMetadata data = imageWriter.getDefaultImageMetadata(new ImageTypeSpecifier(bufferedImage), jpegParams);
+			Element tree = (Element)data.getAsTree("javax_imageio_jpeg_image_1.0");
+			Element jfif = (Element)tree.getElementsByTagName("app0JFIF").item(0);
+			jfif.setAttribute("Xdensity", Integer.toString(dpi));
+			jfif.setAttribute("Ydensity", Integer.toString(dpi));
+			jfif.setAttribute("resUnits", "1"); // density is dots per inch                 
+			data.mergeTree("javax_imageio_jpeg_image_1.0",tree);
+	
+			// Write and clean up
+			imageWriter.write(data, new IIOImage(bufferedImage, null, data), jpegParams);
+			ios.close();
+			imageWriter.dispose();
+		}catch( Exception e ) {
+			outputFile = File.createTempFile("optimizeByFile", ".png");
+			write(bufferedImage,outputFile.getAbsolutePath());
+			e.printStackTrace();
+		}
+		outputFile.deleteOnExit();
+		return outputFile.getAbsolutePath();
 	}
 	
 	public static String optimizeByFileOLD(BufferedImage bufferedImage, float quality, int dpi, int width, int height, boolean gray) throws IOException {
@@ -327,67 +321,79 @@ public class ImageUtil {
 	    }
 	}
 	
-	public static BufferedImage optimize(BufferedImage bufferedImage, float quality, int dpi, int width, int height, boolean gray) throws IOException {
-		File file = new File(optimizeByFile(bufferedImage, quality, dpi, width, height, gray));
-		return load(file);
-	}
+	public static BufferedImage resize(BufferedImage source, float ratio) {
+        BufferedImage target = null;
+        double sx = (double) ratio * source.getWidth();
+        double sy = (double) ratio * source.getHeight();
+        int targetW = source.getWidth();
+        int targetH = source.getHeight();
+        if( sx > 0 ) {
+            targetW = (int)sx;
+        } 
+        if( sy > 0 ){
+            targetH = (int)sy;
+        }
+        target = new BufferedImage(targetW, targetH, source.getType());
+        Graphics2D g = target.createGraphics();
+        // smoother than exlax:
+        g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+        g.drawRenderedImage(source, AffineTransform.getScaleInstance(ratio, ratio));
+        g.dispose();
+        return target;
+    }
 	
-	public static BufferedImage load( File file ) throws IOException {
-		return ImageIO.read(file);
+	public static BufferedImage resize(BufferedImage source, int targetW, int targetH) {
+
+        int type = source.getType();
+        BufferedImage target = null;
+        double sx = (double) targetW / source.getWidth();
+        double sy = (double) targetH / source.getHeight();
+        if (sx < sy) {
+            sx = sy;
+            targetW = (int) (sx * source.getWidth());
+        } else {
+            sy = sx;
+            targetH = (int) (sy * source.getHeight());
+        }
+
+        target = new BufferedImage(targetW, targetH, BufferedImage.TYPE_INT_BGR);
+
+        Graphics2D g = target.createGraphics();
+        // smoother than exlax:
+        g.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+                RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+        g.drawRenderedImage(source, AffineTransform.getScaleInstance(sx, sy));
+        g.dispose();
+        return target;
+    }
+	
+	public static BufferedImage toGrayScale( BufferedImage source ) {
+		BufferedImage result = new BufferedImage(
+			source.getWidth(),
+			source.getHeight(),
+            BufferedImage.TYPE_INT_RGB);
+        Graphics2D graphic = result.createGraphics();
+        graphic.drawImage(source, 0, 0, Color.WHITE, null);
+        for (int i = 0; i < result.getHeight(); i++) {
+            for (int j = 0; j < result.getWidth(); j++) {
+                Color c = new Color(result.getRGB(j, i));
+                int red = (int) (c.getRed() * 0.299);
+                int green = (int) (c.getGreen() * 0.587);
+                int blue = (int) (c.getBlue() * 0.114);
+                Color newColor = new Color(
+	                red + green + blue,
+	                red + green + blue,
+	                red + green + blue);
+                result.setRGB(j, i, newColor.getRGB());
+            }
+        }
+        return result;
 	}
 	
 	public static File write(BufferedImage bufferedImage, String fileName) throws IOException {
 		File outputfile = new File(fileName);
 	    ImageIO.write(bufferedImage, fileName.substring(fileName.lastIndexOf(".")+1).toLowerCase(), outputfile);
 	    return outputfile;
-	}
-	
-	public static String getFormatName( String fileName ) throws IOException {
-		File sourceFile = new File(fileName);
-		ImageInputStream iis = ImageIO.createImageInputStream(sourceFile);
-		Iterator<ImageReader> imageReaders = ImageIO.getImageReaders(iis);
-		ImageReader ir = null;
-		while( imageReaders.hasNext() ) {
-			ir = imageReaders.next();
-		}
-		sourceFile = null;
-		return ir != null ? ir.getFormatName() : null;
-	}
-	
-	public static String optimizeByFile(BufferedImage bufferedImage, float quality, int dpi, int width, int height, boolean gray) throws IOException {
-		File outputFile = File.createTempFile("pdfimage", ".jpeg");
-		/*
-		ImageInputStream iis = ImageIO.createImageInputStream(bufferedImage);
-		Iterator<ImageReader> imageReaders = ImageIO.getImageReaders(iis);
-		ImageReader ir = null;
-		while( imageReaders.hasNext() ) {
-			ir = imageReaders.next();
-			System.out.println("ImageReader: "+ir.getFormatName());
-		}
-		ImageWriter imageWriter = ImageIO.getImageWritersBySuffix(ir.getFormatName().toLowerCase()).next();
-		*/
-		ImageWriter imageWriter = ImageIO.getImageWritersBySuffix("jpeg").next();
-		ImageOutputStream ios = ImageIO.createImageOutputStream(outputFile);
-		imageWriter.setOutput(ios);
-		ImageWriteParam jpegParams = imageWriter.getDefaultWriteParam();
-		
-		jpegParams.setCompressionMode(JPEGImageWriteParam.MODE_EXPLICIT);
-		jpegParams.setCompressionQuality(quality);
-
-		IIOMetadata data = imageWriter.getDefaultImageMetadata(new ImageTypeSpecifier(bufferedImage), jpegParams);
-		Element tree = (Element)data.getAsTree("javax_imageio_jpeg_image_1.0");
-		Element jfif = (Element)tree.getElementsByTagName("app0JFIF").item(0);
-		jfif.setAttribute("Xdensity", Integer.toString(dpi));
-		jfif.setAttribute("Ydensity", Integer.toString(dpi));
-		jfif.setAttribute("resUnits", "1"); // density is dots per inch                 
-		data.mergeTree("javax_imageio_jpeg_image_1.0",tree);
-
-		// Write and clean up
-		imageWriter.write(data, new IIOImage(bufferedImage, null, data), jpegParams);
-		ios.close();
-		imageWriter.dispose();
-		outputFile.deleteOnExit();
-		return outputFile.getAbsolutePath();
 	}
 	
 }
